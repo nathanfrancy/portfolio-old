47dc7acbfc34a17d835b5df2e49c3ca970859e26
Nathan Francy <nathanfrancy@gmail.com>
2014-05-26 23:58:02 -0500

    a couple changes
    *add gh
    *remove linkedin, facebook
    *font size change on smaller sizes - bigger now
    *center font when smaller screen size
    *adding gitignore

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
 	<div id="nat-footer">
 		<div class="social-box">
-            <div class="social social-f"><a href="http://www.facebook.com/nathanfrancy">f</a></div>
             <div class="social social-t"><a href="https://twitter.com/nathanfrancy">t</a></div>
-            <div class="social social-l"><a href="http://www.linkedin.com/pub/nathan-francy/50/547/862/">l</a></div>
+            <div class="social social-g"><a href="http://www.github.com/nathanfrancy">g</a></div>
             <div class="clear"></div>
         </div>
 	</div>
